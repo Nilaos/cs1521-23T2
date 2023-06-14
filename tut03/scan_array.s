b30ba0609539eb5e9617587b34e989606de7ff98
Nicholas Sims <nicholas.d.sims@gmail.com>
2023-06-14 16:04:13 +1000

    updated scan_array example

--- tut03/scan_array.s
+++ tut03/scan_array.s
@@ -28,13 +28,15 @@ scan_loop_cond:
 	# TODO: calculate &(numbers[i]) and store the value in $v0 into it
 	# Hint: Is there a simple formula to use here?
 	# 1) Where does the array start?
-	la	$t2,	numbers		# Start of array
+	la	$t2,	numbers		# Find base address of start of array
 	# 2) How far into the array is the element, in bytes?
-	mul	$t3,	$t0,	4	# sizeof(elem) * i
+	mul	$t3,	$t0,	4	# Calculate array offset as sizeof(elem) * i
 	# 3) What does this make the address of that element?
-	add	$t4,	$t2,	$t3	# finding the address of numbers[i]
+	add	$t4,	$t3,	$t2	# Address of numbers[i] = base address + offset
 	# 4) How can we store this into the array?
-	sw	$t1,	($t4)
+	sw	$t1,	($t4)		# Store a value into numbers[i], now we have the address
+	
+	sw	$t1,	numbers($t3)	# Alternative way, which does step 3 for you
 
 					# Jump back to the loop condition
 	addi    $t0, $t0, 1             # i++;
